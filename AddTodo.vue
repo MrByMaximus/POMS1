b409f6fbbf640203d6cdd2ff3123ff20399deb09
MrByMaximus <72687937+MrByMaximus@users.noreply.github.com>
2022-10-04 16:21:36 +0700

    Add files via upload

---
+++ AddTodo.vue
@@ -0,0 +1,41 @@
+<template>
+    <form>
+        <input type="text">
+        <button type="submit">Create</button>
+    </form>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                title: ''
+            }
+        },
+        methods: {
+            onSubmit() {
+                console.log('Submit', this.title)
+                if (this.title.trim()) {
+                    const newTodo = {
+                        id: Date.now(),
+                        title: this.title,
+                        completed: false
+                    }
+
+                    this.$emit('add-todo', newTodo)
+                    this.title = ''
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    form {
+        display: flex;
+    }
+
+    input {
+        width: 400px;
+    }
+</style>
---
+++ TodoList.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <ul>
+            <TodoItem
+                v-for="todo of todos"
+                v-bind:todo="todo"
+                v-bind:index="i"
+                v-on:todo-remove="removeTodo"
+            />
+        </ul>
+    </div>
+</template>
+
+<script>
+    import TodoItem from '@/components/TodoItem'
+    export default {
+        props: ['todos'],
+        components: {
+            TodoItem
+        },
+        methods: {
+            removeTodo(id) {
+                // console.log(id)
+                this.$emit('remove-todo', id)
+            }
+        }
+    }
+</script>
